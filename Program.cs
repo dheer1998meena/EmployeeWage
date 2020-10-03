b78aea08c2951ab02a74c499a3dd80d9507ed0ef
dheer meena <dheer1998meena@gmail.com>
2020-10-03 14:06:25 +0530

    [Dheer] Add . added  UC

--- Program.cs
+++ Program.cs
@@ -10,26 +10,34 @@ namespace EmployeeWage
 
             const int EMP_FULL_TIME = 1;
             const int EMP_PART_TIME = 2;
+            const int NUM_OF_WORKING_DAY = 20;
             int EMP_RATE_PER_HOUR = 20;
 
+            int totalEmpWage = 0;
             int empWage = 0;
             int empHrs = 0;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+
+            for (int Day = 0; Day < NUM_OF_WORKING_DAY; Day++)
             {
-                case EMP_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                case EMP_PART_TIME:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case EMP_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case EMP_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("empWage : " + empWage);
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("empWage : " + empWage);
+            Console.WriteLine("Total Emp Wage :" + totalEmpWage);
         }
     }
 }
