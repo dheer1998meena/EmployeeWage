d4b7523a7c2fa65e4a16cfb2289e88893e07f5df
dheer meena <dheer1998meena@gmail.com>
2020-10-04 22:33:27 +0530

    [Dheer] Add . added UC10

---
+++ Company.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWage
+{
+
+    public class Company
+    {
+        public string companyName;
+        public int empRatePerHour;
+        public int maxWorkingDays;
+        public int maxWorkingHours;
+        public int totalEmpWage;
+
+        public Company(string companyName, int empRatePerHour, int maxWorkingDays, int maxWorkingHours)
+        {
+            this.companyName = companyName;
+            this.empRatePerHour = empRatePerHour;
+            this.maxWorkingDays = maxWorkingDays;
+            this.maxWorkingHours = maxWorkingHours;
+        }
+        public void SaveTotalWage(int totalWage)
+        {
+            totalEmpWage = totalWage;
+        }
+
+    }
+
+
+}
+
+	
